1c5bcc8d00641b6b3721feccf71d404b8bd77f3d
twok1 <twokey25@gmail.com>
2025-09-16 14:05:50 +0300

    added log to file

--- core/data_writer.py
+++ core/data_writer.py
@@ -7,13 +7,20 @@ class DataWriter:
         self.target = target
         self.cabinet_jumpers: Dict[str, List[Set[str]]] = cabinet_jumpers
         self.jumpers_to_lines: Dict[str, Dict[str, List[str]]] = jumpers_to_lines
+        with open(self.target, 'w', encoding='utf-8') as f:
+            f.write('')
         
     def process(self):
         for cabinet, jumpers in sorted(self.cabinet_jumpers.items(), key=lambda x: int(''.join(i for i in x[0] if i.isdigit()))):
-            print(cabinet)
+            self.print(cabinet)
             for jumper in sorted(jumpers, key=sorting_key):
                 lines = []
                 for wire in jumper:
                     lines.append(', '.join(self.jumpers_to_lines[cabinet][wire]))
-                print('\t', jumper.tabulated_term())
-                print('\t', '\t'.join(lines))
+                self.print(f'\t{jumper.tabulated_term()}')
+                self.print(f'\t{"\t".join(lines)}')
+                
+    def print(self, *line):
+        with open(self.target, 'a', encoding='utf-8') as f:
+            f.write(f'{line[0]}\n')
+            print(*line)
